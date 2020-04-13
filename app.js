6da752af4f59066f64c9f6f7adefbbd003177b12
강단 <kkangdanni@sk.com>
2020-04-13 11:21:11 +0900

    StrokeColor Handling

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -26,14 +27,15 @@ function onMouseMove(event) {
     ctx.moveTo(x, y);
   } else {
     ctx.lineTo(x, y);
-    console.log("line", x, y);
+    //console.log("line", x, y);
     ctx.stroke();
   }
 }
 
-function onMouseDown(event) {
-  //console.log(event);
-  painting = true;
+function handleColorClick(event) {
+  //console.log(event.target.style);
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
 }
 
 if (canvas) {
@@ -42,3 +44,5 @@ if (canvas) {
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach((color) => color.addEventListener("click", handleColorClick));

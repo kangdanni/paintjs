10193ae58fd507c7eeb942ffb452d200f4bc2d52
강단 <kkangdanni@sk.com>
2020-04-13 10:42:38 +0900

    canvas SIZE 오류 수정

--- app.js
+++ app.js
@@ -20,11 +20,13 @@ function startPainting() {
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
+  //console.log(x, y);
   if (!painting) {
     ctx.beginPath();
     ctx.moveTo(x, y);
   } else {
     ctx.lineTo(x, y);
+    console.log("line", x, y);
     ctx.stroke();
   }
 }
